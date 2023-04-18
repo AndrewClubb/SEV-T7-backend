2ad0cf8e1e87eab8aa1e711670a44a9471a71f95
Andrew Clubb <andrew.clubb@eagles.oc.edu>
2023-04-17 21:42:13 -0500

    user table knows last role

--- app/controllers/auth.controller.js
+++ app/controllers/auth.controller.js
@@ -130,6 +130,7 @@ exports.login = async (req, res) => {
             fName: user.fName,
             lName: user.lName,
             userId: user.id,
+            lastRole: user.lastRole,
             token: session.token,
             // refresh_token: user.refresh_token,
             // expiration_date: user.expiration_date
@@ -171,6 +172,7 @@ exports.login = async (req, res) => {
           fName: user.fName,
           lName: user.lName,
           userId: user.id,
+          lastRole: user.lastRole,
           token: token,
           // refresh_token: user.refresh_token,
           // expiration_date: user.expiration_date
--- app/models/user.model.js
+++ app/models/user.model.js
@@ -1,28 +1,33 @@
 module.exports = (sequelize, Sequelize) => {
-  const User = sequelize.define("user", {
-    id: {
-      type: Sequelize.INTEGER,
-      autoIncrement: true,
-      primaryKey: true,
-      unique: true
+  const User = sequelize.define(
+    "user",
+    {
+      id: {
+        type: Sequelize.INTEGER,
+        autoIncrement: true,
+        primaryKey: true,
+        unique: true,
+      },
+      fName: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      lName: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      email: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      lastRole: {
+        type: Sequelize.STRING,
+      },
     },
-    fName: {
-      type: Sequelize.STRING,
-      allowNull: false
-    },
-    lName: {
-      type: Sequelize.STRING,
-      allowNull: false
-    },
-    email: {
-      type: Sequelize.STRING,
-      allowNull: false
-    },
-  },
-  {
-    timestamps: false
-  });
+    {
+      timestamps: false,
+    }
+  );
 
   return User;
 };
-
